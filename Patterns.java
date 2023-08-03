65f3e64c4b9d785f6e5d4d134c969487b70989b4
Bhojanapu Harsha vardhan Kumar <119922494+harshakohli645@users.noreply.github.com>
2023-08-03 12:49:28 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,20 @@
+public class Patterns 
+{
+    public static void main(String[]args)
+    {
+        int i;
+        int j;
+        int n=5;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                System.out.print("*");
+                
+        
+            }
+            
+            System.out.println();
+        }
+    }
+}
